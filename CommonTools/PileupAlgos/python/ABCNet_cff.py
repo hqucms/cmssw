6d2ed9930788ecf4faf85e2b3849c7ad531bb617
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-05-23 11:33:03 +0200

    add Run2 model stored in protobuf file (saved with cmsml module)

--- CommonTools/PileupAlgos/python/ABCNet_cff.py
+++ CommonTools/PileupAlgos/python/ABCNet_cff.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 abc = cms.EDProducer("ABCNetProducer",
                      candName = cms.InputTag("packedPFCandidates"),
-                     graph_path = cms.FileInPath("toy/path/for/now/model.pb")
+                     graph_path = cms.FileInPath("CommonTools/PileupAlgos/plugins/AttentionBasedPileupRejectionModel_Run2.pb")
 )
